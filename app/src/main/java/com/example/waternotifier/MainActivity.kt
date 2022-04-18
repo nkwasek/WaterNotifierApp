e90d5992c4668f34ad3660c3395c1445b2b9dd0d
Natalia Kwasek <n.kwasek@stud.elka.pw.edu.pl>
2022-04-18 22:43:19 +0200

    activity_goal prototype

--- app/src/main/java/com/example/waternotifier/MainActivity.kt
+++ app/src/main/java/com/example/waternotifier/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.waternotifier
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_goal)
+        val spinner: Spinner = findViewById(R.id.spinner_sex)
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.sex_array,
+            R.layout.spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(R.layout.spinner_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
     }
 }
