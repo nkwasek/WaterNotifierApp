fd2147997624fae84c43e42908884df50e091c5f
Natalia Kwasek <n.kwasek@stud.elka.pw.edu.pl>
2022-04-19 16:14:04 +0200

    activity_login and activity_goal update

--- app/src/main/java/com/example/waternotifier/MainActivity.kt
+++ app/src/main/java/com/example/waternotifier/MainActivity.kt
@@ -4,23 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_goal)
-        val spinner: Spinner = findViewById(R.id.spinner_sex)
-// Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.sex_array,
-            R.layout.spinner_item
+        initGoalSpinners()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+    }
+
+    fun initGoalSpinners()
+    {
+        val spinnerSex: Spinner = findViewById(R.id.spinner_sex)
+        val spinnerLifestyle: Spinner = findViewById(R.id.spinner_lifestyle)
+
+        ArrayAdapter.createFromResource(this, R.array.sex_array, R.layout.spinner_item
         ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(R.layout.spinner_item)
-            // Apply the adapter to the spinner
-            spinner.adapter = adapter
+            adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown)
+            spinnerSex.adapter = adapter
         }
 
+        ArrayAdapter.createFromResource(this, R.array.lifestyle_array, R.layout.spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown)
+            spinnerLifestyle.adapter = adapter
+        }
     }
 }
