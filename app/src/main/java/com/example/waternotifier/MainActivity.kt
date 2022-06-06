a6ab1dd01134437482e2f4566d208d16f7e2ab43
Natalia Kwasek <n.kwasek@stud.elka.pw.edu.pl>
2022-06-06 11:05:45 +0200

    First clicks settings

--- app/src/main/java/com/example/waternotifier/MainActivity.kt
+++ app/src/main/java/com/example/waternotifier/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.example.waternotifier
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.ImageButton
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        setContentView(R.layout.activity_main)
         //initGoalSpinners()
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        val settingsButton = findViewById<ImageButton>(R.id.settingsButton)
+        settingsButton.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
 
     }
 
