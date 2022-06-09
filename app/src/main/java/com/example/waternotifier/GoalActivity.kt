e4a181b582b239bf168073cda7ed8c06205c8dbd
Natalia Kwasek <nkwasek1@gmail.com>
2022-06-09 13:07:42 +0200

    Daily water goal

--- app/src/main/java/com/example/waternotifier/GoalActivity.kt
+++ app/src/main/java/com/example/waternotifier/GoalActivity.kt
@@ -2,9 +2,7 @@ package com.example.waternotifier
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class GoalActivity : AppCompatActivity() {
@@ -14,11 +12,44 @@ class GoalActivity : AppCompatActivity() {
         initGoalSpinners()
 
         val okButton = findViewById<Button>(R.id.goalOkButton)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+        val minusButton = findViewById<ImageButton>(R.id.minusButton)
+        val plusButton = findViewById<ImageButton>(R.id.plusButton)
+        val dailyWater = findViewById<TextView>(R.id.dailyWater)
+
+        var dailyValue = Integer.parseInt(dailyWater.text.split(' ')[0])
+
         okButton.setOnClickListener{
             val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
             finish()
         }
+
+        minusButton.setOnClickListener{
+            if (dailyValue<100) dailyValue=0 else dailyValue-=100
+            seekBar.setProgress(dailyValue)
+            dailyWater.text=dailyValue.toString() + " ml"
+        }
+
+        plusButton.setOnClickListener{
+            if (dailyValue>4900) dailyValue=5000 else dailyValue+=100
+            seekBar.setProgress(dailyValue)
+            dailyWater.text=dailyValue.toString() + " ml"
+        }
+
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                    dailyValue = progress
+                    dailyWater.text = dailyValue.toString() + " ml"
+                }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        }
+        )
     }
 
     override fun onBackPressed() {
