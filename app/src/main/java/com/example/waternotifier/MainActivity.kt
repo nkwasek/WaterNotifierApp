fba4d2aa0d202dfcaa9ac6001f3994c7095261cf
Natalia Kwasek <n.kwasek@stud.elka.pw.edu.pl>
2022-06-13 18:56:32 +0200

    Loading screen fix

--- app/src/main/java/com/example/waternotifier/MainActivity.kt
+++ app/src/main/java/com/example/waternotifier/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.waternotifier
 
+import android.content.ContentValues
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.widget.ImageButton
+import android.util.Log
+import android.view.View
 import android.widget.TextView
+import androidx.annotation.Nullable
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val db = Firebase.firestore
+    private val uid = Firebase.auth.currentUser?.uid.toString()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_loading_screen)
+
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
@@ -31,8 +42,28 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
                 finish()
             }
-        }, 5000)
+        }, 1000)
 
+    }
+
+    fun readData() {
+        db.collection(uid.toString())
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    Log.d(ContentValues.TAG, "${document.id} => ${document.data}")
+                }
+                val progress = findViewById<TextView>(R.id.progressTextView)
+
+                if(documents.count() > 0) {
+                    LocalVariables.Progress = Integer.parseInt(documents.last().data.get("progress").toString())
+                    LocalVariables.Goal = Integer.parseInt(documents.last().data.get("goal").toString())
+                }
+
+            }
+            .addOnFailureListener { exception ->
+                Log.w(ContentValues.TAG, "Error getting documents: ", exception)
+            }
     }
 
 
